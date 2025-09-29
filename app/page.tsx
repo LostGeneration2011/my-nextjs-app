35b72b86def6b0c54b91f5685b3bb8692108a853
anhdinh <anhdinh0408@gmail.com>
2025-09-29 21:11:33 +0700

    Update Source Code with path

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export interface Props{
   myName: string;
 }
 
-export default function Page({myName = "John"}: Props) {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -22,7 +22,7 @@ export default function Page({myName = "John"}: Props) {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>My name is: {myName}</strong>
+            
           </p>
           <InvoiceStatus status={"paid"} />
           <Link
@@ -46,7 +46,7 @@ export default function Page({myName = "John"}: Props) {
           height={620}
           className="block md:hidden"
           />
-          
+
         </div>
       </div>
     </main>
